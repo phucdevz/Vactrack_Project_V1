464216f1c6c648397be63917472bd29b7a91e18a
Truong Phuc <151916898+phucdevz@users.noreply.github.com>
2025-03-25 22:22:37 +0700

    Add files via upload

---
+++ Vactrack_BackEnd/src/main/java/com/vactrack/VacTrackApplication.java
@@ -0,0 +1,11 @@
+package com.vactrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacTrackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VacTrackApplication.class, args);
+    }
+}
