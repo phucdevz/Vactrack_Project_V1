81130fe9291d1f426096be686b133b290b234497
Truong Phuc <151916898+phucdevz@users.noreply.github.com>
2025-04-21 09:44:04 +0700

    Add files via upload

---
+++ Vactrack_BackEnd/src/main/java/com/vactrack/model/Message.java
@@ -0,0 +1,27 @@
+package com.vactrack.model;
+
+public class Message {
+    private String role;
+    private String content;
+
+    public Message() {}
+
+    public Message(String role, String content) {
+        this.role = role;
+        this.content = content;
+    }
+
+    // Getters và Setters
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
